8e8e744010d3258931e9ccf46a744eed72899c84
Richish Mandal <richishm19@gmail.com>
2022-09-14 22:56:46 +0530

    Home Page Completed

--- lib/pages/bottom_bar.dart
+++ lib/pages/bottom_bar.dart
@@ -1,3 +1,4 @@
+import 'package:booktickets/pages/home_page.dart';
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 
@@ -9,26 +10,53 @@ class BottomBar extends StatefulWidget {
 }
 
 class _BottomBarState extends State<BottomBar> {
+  int _currentIndex = 0;
+  static final List<Widget> _barOptions = <Widget>[
+    HomePage(),
+    const Text("Search"),
+    const Text("Ticket"),
+    const Text("Profile"),
+  ];
+
+  void onTapItem(int selectedIndex) {
+    setState(() {
+      _currentIndex = selectedIndex;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("My Tickets"),
       ),
-      body: const Center(
-          child: Text("My Body")
-      ),
+      body: Center(child: _barOptions[_currentIndex]),
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: onTapItem,
         selectedItemColor: Colors.blueGrey,
         unselectedItemColor: Colors.blueGrey.withOpacity(0.6),
         elevation: 10,
         showSelectedLabels: false,
         showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
         items: const [
-          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_home_regular), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_home_regular), label: "Search"),
-          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_home_regular), label: "Ticket"),
-          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_home_regular), label: "Profile"),
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+              label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+              label: "Search"),
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
+              label: "Ticket"),
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+              label: "Profile"),
         ],
       ),
     );
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFeeedf2),
+      body: ListView(
+        children: [
+          Container(
+            child: Column(),
+          )
+        ],
+      ),
+    );
+  }
+}
